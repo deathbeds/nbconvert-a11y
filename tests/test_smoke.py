0d60f01fff0285dd51db0c84f10d38c3c8229ea6
tonyfast <tony.fast@gmail.com>
2023-12-01 12:04:19 -0800

    include new lsit configuration in tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -50,6 +50,7 @@ configs = mark.parametrize(
     [
         param((CONFIGURATIONS / (id := "a11y")).with_suffix(".py"), id=id),
         param((CONFIGURATIONS / (id := "section")).with_suffix(".py"), id=id),
+        param((CONFIGURATIONS / (id := "list")).with_suffix(".py"), id=id),
         param(
             (CONFIGURATIONS / (id := "default")).with_suffix(".py"),
             id=id,
