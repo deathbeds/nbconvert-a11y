eaffedc18cb1c87b0fabed95ae9a8b045f9d9fea
tonyfast <tony.fast@gmail.com>
2023-12-06 19:42:40 -0800

    force black background for dark theme tests

--- tests/test_color_themes.py
+++ tests/test_color_themes.py
@@ -1,5 +1,5 @@
 from nbconvert import get_exporter
-from pytest import fixture, mark, param
+from pytest import fixture, mark, param, xfail
 
 from nbconvert_a11y.pytest_axe import JUPYTER_WIDGETS, MATHJAX, PYGMENTS, AllOf, Axe, Violation
 from tests.test_smoke import CONFIGURATIONS, NOTEBOOKS, get_target_html
@@ -9,27 +9,29 @@ THEMES = ["a11y", "a11y-high-contrast", "gh", "gh-colorblind", "gh-high", "gotth
 
 
 @fixture(params=THEMES)
-def lorenz(page, tmp_path, request):
+def exporter(request):
     e = get_exporter("a11y")()
     e.color_theme = request.param
     e.wcag_priority = "AA"
-    html, *_ = e.from_filename(LORENZ)
-    tmp = tmp_path / f"{request.param}.html"
-    tmp.write_text(html)
-    axe = Axe(page=page, url=tmp.absolute().as_uri())
-    yield axe.configure()
+    yield e
 
 
-@mark.xfail(
-    reason="""serious color contrast issues are raised on dark theme because 
-axe is comparing colors to a white background in dark mode.""",
-    raises=AllOf
-)
+@fixture
+def lorenz(page, tmp_path, exporter):
+    tmp = tmp_path / f"{exporter.color_theme}.html"
+    tmp.write_text(exporter.from_filename(LORENZ)[0])
+    yield Axe(page=page, url=tmp.absolute().as_uri()).configure()
+
+
 def test_dark_themes(lorenz):
     lorenz.page.click("""[aria-controls="nb-settings"]""")
     lorenz.page.locator("select[name=color-scheme]").select_option("dark mode")
     lorenz.page.keyboard.press("Escape")
-    raise lorenz.run(dict(include=[".nb-source"])).raises_allof(Violation["serious-color-contrast"])
+    # force the background to be black because axe detects a white background in dark mode.
+    lorenz.page.eval_on_selector("body", """x => x.style.backgroundColor = `#000000`""")
+    lorenz.run(dict(include=[".nb-source"])).raises()
+    # accessible pygments disclsoes that we should expect some color contrast failures on some themes.
+    # there isnt much code which might not generate enough conditions to create color contrast issues.
 
 
 def test_light_themes(lorenz):
