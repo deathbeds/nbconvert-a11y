f5afe06eb94e3b99295e19c6d4716d4c9a6563db
tonyfast <tony.fast@gmail.com>
2023-12-12 14:23:23 -0800

    use a live server to test the exported documents

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,73 @@
+from ast import literal_eval
+import urllib
+from os import environ
+from pathlib import Path
 from unittest import TestLoader
 
+import pytest
+from flask import Flask, request, url_for
+
+from nbconvert import get_exporter
 
 collect_ignore = ["notebooks/lorenz.ipynb"]
+HERE = Path(__file__).parent
+CONFIGURATIONS = HERE / "configurations"
+NOTEBOOKS = HERE / "notebooks"
+EXPORTS = HERE / "exports"
+HTML = EXPORTS / "html"
+CI = environ.get("CI")
 
 TestLoader.testMethodPrefix = "test", "xfail"
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if "app" in getattr(item, "fixturenames", ()):
+            item.add_marker(pytest.mark.options(debug=True))
+
+
+def format_qs_value(x):
+    for x in map(bytes.decode, x):
+        if x in {"True", False}:
+            return literal_eval(x)
+        if x.isnumeric():
+            return float(x)
+        return x
+
+
+def make_app():
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+
+    @app.route("/")
+    def list():
+        return "\n".join(map(str, NOTEBOOKS.glob("*.ipynb")))
+
+    @app.route("/<exporter>/<name>")
+    def nb(exporter, name):
+        e = get_exporter(exporter)(
+            **{
+                k.decode(): format_qs_value(v)
+                for k, v in urllib.parse.parse_qs(request.query_string).items()
+            }
+        )
+        return e.from_filename(NOTEBOOKS / name)[0]
+
+    return app
+
+
+@pytest.fixture(scope="session")
+def app():
+    return make_app()
+
+
+@pytest.fixture(scope="session")
+def notebook(live_server):
+    def url(exporter="a11y", name="lorenz-executed.ipynb", **qs):
+        return url_for("nb", exporter=exporter, name=name, **qs, _external=True)
+
+    return url
+
+
+if __name__ == "__main__":
+    make_app().run(debug=True, port=5000)
--- tests/test_third.py
+++ tests/test_third.py
@@ -8,10 +8,10 @@ from functools import partial
 from os import environ
 from unittest import TestCase
 
+import pytest
 from pytest import fixture, mark, skip
 
 from nbconvert import get_exporter
-import pytest
 from nbconvert_a11y.pytest_axe import (
     JUPYTER_WIDGETS,
     NO_ALT,
@@ -48,12 +48,18 @@ class DefaultTemplate(TestCase):
     # then incrementally explain them in smaller tests.
     def xfail_all(self):
         exceptions = self.axe.run().results.exception()
-        try: raise exceptions
-        except* Violation["critical-image-alt"]: ...
-        except* Violation["serious-color-contrast-enhanced"]: ...
-        except* Violation["serious-aria-input-field-name"]: ...
-        except* Violation["serious-color-contrast"]: ...
-        except* Violation["minor-focus-order-semantics"]: ...
+        try:
+            raise exceptions
+        except* Violation["critical-image-alt"]:
+            ...
+        except* Violation["serious-color-contrast-enhanced"]:
+            ...
+        except* Violation["serious-aria-input-field-name"]:
+            ...
+        except* Violation["serious-color-contrast"]:
+            ...
+        except* Violation["minor-focus-order-semantics"]:
+            ...
         pytest.xfail("there are 1 critical, 3 serious, and 1 minor accessibility violations")
 
     def xfail_pygments_highlight_default(self):
@@ -63,37 +69,44 @@ class DefaultTemplate(TestCase):
         """
         # further verification would testing the nbviewer layer.
         exceptions = self.axe.run({"include": [PYGMENTS]}).results.exception()
-        try: raise exceptions
-        except* Violation["serious-color-contrast-enhanced"]: ...
-        except* Violation["serious-color-contrast"]: ...
+        try:
+            raise exceptions
+        except* Violation["serious-color-contrast-enhanced"]:
+            ...
+        except* Violation["serious-color-contrast"]:
+            ...
         pytest.xfail("there are 2 serious color contrast violations.")
-
 
     def xfail_widget_display(self):
         """The simple lorenz widget generates one minor and one serious accessibility violation."""
-        exceptions = self.axe.run({"include": [JUPYTER_WIDGETS], "exclude": [NO_ALT]}).results.exception()
-        try: raise exceptions
-        except* Violation["minor-focus-order-semantics"]: ...
-        except* Violation["serious-aria-input-field-name"]: ...
+        exceptions = self.axe.run(
+            {"include": [JUPYTER_WIDGETS], "exclude": [NO_ALT]}
+        ).results.exception()
+        try:
+            raise exceptions
+        except* Violation["minor-focus-order-semantics"]:
+            ...
+        except* Violation["serious-aria-input-field-name"]:
+            ...
         pytest.xfail("widgets have not recieved a concerted effort.")
 
     @fixture(autouse=True)
-    def lorenz(self, axe, tmp_path, exporter):
-        tmp = (tmp_path / LORENZ.name).with_suffix(".html")
-        tmp.write_text(exporter.from_filename(LORENZ)[0])
-        self.axe = axe(tmp.as_uri().strip()).configure()
-
+    def url(self, axe, notebook):
+        self.axe = axe(notebook("html", "lorenz-executed.ipynb")).configure()
 
 class A11yTemplate(TestCase):
     def xfail_sa11y(self):
         """The simple lorenz widget generates one minor and one serious accessibility violation."""
         exceptions = self.axe.run({"include": [SA11Y]}).results.exception()
-        try: raise exceptions
-        except *Violation["serious-label-content-name-mismatch"]: ...
+        try:
+            raise exceptions
+        except* Violation["serious-label-content-name-mismatch"]:
+            ...
         pytest.xfail("an issue report needs to be filed with sa11y.")
 
     @fixture(autouse=True)
-    def lorenz(self, axe, tmp_path, a11y_exporter):
-        tmp = (tmp_path / LORENZ.name).with_suffix(".html")
-        tmp.write_text(a11y_exporter.from_filename(LORENZ)[0])
-        self.axe = axe(tmp.as_uri().strip()).configure()
+    def url(self, axe, notebook):
+        self.axe = axe(notebook("a11y", "lorenz-executed.ipynb", **{
+            "include_sa11y": True
+        })).configure()
+        print(self.axe.url)
