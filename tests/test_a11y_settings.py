06392bac2b6709a8cad3f2f209dbfd9f5ec25cd4
tonyfast <tony.fast@gmail.com>
2024-02-18 21:37:10 -0800

    remove audit test

--- tests/test_a11y_settings.py
+++ tests/test_a11y_settings.py
@@ -23,7 +23,6 @@ def lorenz(page, notebook):
         "[aria-controls=nb-settings]",
         "[aria-controls=nb-help]",
         "[aria-controls=nb-metadata]",
-        "[aria-controls=nb-audit]",
         param("[aria-controls=nb-expanded-dialog]", marks=mark.xfail(reason=NEEDS_WORK)),
         param("[aria-controls=nb-visibility-dialog]", marks=mark.xfail(reason=NEEDS_WORK)),
         # param("nada", marks=mark.xfail(reason="no selector")),
